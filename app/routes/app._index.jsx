7abb2b3f5df947bda5c16eccbb0c82e38fe86864
Luiz Ricardo <ricardo_santos-2010@hotmail.com>
2024-03-13 20:40:48 -0300

    fix index layout design principles following design polaris guide

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,29 +1,21 @@
 import { json } from "@remix-run/node";
 import {
   useLoaderData,
-  Link,
   useNavigate,
   useActionData,
 } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
 import { 
   BlockStack,
-  Box,
   Card,
-  Button,
   Layout,
   Page,
   Text,
-  ResourceList,
-  ResourceItem,
   Badge,
   Thumbnail,
   SkeletonThumbnail,
-  InlineStack,
-  InlineGrid,
-  ButtonGroup,
+  IndexTable
 } from "@shopify/polaris";
-import {PlusIcon} from '@shopify/polaris-icons';
 import db from "../db.server";
 
 export async function loader({ request }) {
@@ -47,65 +39,89 @@ export default function Index() {
   const navigate = useNavigate();
   const { tables } = useLoaderData();
 
-  function renderItem(item) {
-    const { id, title, content, status, type } = item;
-
-    const url = `/app/tableform/${id}`;
-    let media = (<SkeletonThumbnail size="small" />);
-    if (type === "image") {
-      media = (<Thumbnail product size="small" name={title} source={content} />);
-    }
-
-
-    return (
-      <ResourceItem
-        id={id}
-        onClick={() => navigate(url)}
-        accessibilityLabel={`Ver detalhes para ${title}`}
-        verticalAlignment="center"
-        horizontal
-        media={media}
-      >
-        <InlineStack direction="row" align="space-between">
-          <Text variant="bodyMd" fontWeight="bold" as="h3">
-            {title}
-          </Text>
-          <Badge 
-            tone={status ? "success" : "critical" }
-            progress={status ? "complete" : "incomplete"}
-            size="small"
-            align="right"
-          >
-            {status ? "Ativo" : "Inativo"}
-          </Badge>
-        </InlineStack>
-      </ResourceItem>
-    );
+  const resourceName = {
+    singular: "tabela",
+    plural: "tabelas",
   }
 
+  const rowsTables = tables.map(
+    (
+      { id, title, content, status, type },
+      index
+    ) => (
+      <>
+      <IndexTable.Row
+        id={id}
+        key={id}
+        position={index}
+        onClick={() => navigate(`/app/tableform/${id}`)}
+      >
+        <IndexTable.Cell>
+          <Text fontWeight="bold">#{id}</Text>
+        </IndexTable.Cell>
+
+        <IndexTable.Cell>
+          {type === "image" ? (
+            <span class="Polaris-Thumbnail Polaris-Thumbnail--sizeSmall">
+              <img
+                alt={title}
+                src={content}
+                style={{ objectFit: "cover", height: "100%", width: "100%" }}
+              />
+            </span>
+          ) : (
+            <SkeletonThumbnail size="small" />
+          )}
+        </IndexTable.Cell>
+
+        <IndexTable.Cell>
+          <Text as="span">
+            {title}
+          </Text>
+        </IndexTable.Cell>
+
+        <IndexTable.Cell>{type === "image" ? "Imagem" : "Texto"}</IndexTable.Cell>
+
+        <IndexTable.Cell>
+          <Text alignment="end">
+            <Badge 
+              tone={status ? "success" : "critical" }
+              progress={status ? "complete" : "incomplete"}
+              size="small"
+            >
+              {status ? "Ativo" : "Inativo"}
+            </Badge>
+          </Text>
+        </IndexTable.Cell>
+
+      </IndexTable.Row>
+      </>
+    )
+  );
+
   return (
-    <Page>
+    <Page
+      title={"Suas tabelas"}
+      fullWidth
+      primaryAction={{content: 'Criar nova tabela', onAction: () => navigate("/app/tableform/new")}}
+    >
       <Layout>
         <Layout.Section>
-          <Card title="Suas tabelas" roundedAbove="sm">
-            <BlockStack gap="200">
-              <InlineGrid columns="1fr auto">
-                <Text as="h2" variant="headingSm">
-                  Suas tabelas
-                </Text>
-                <ButtonGroup>
-                  <Button icon={PlusIcon} variant="primary" onClick={() => navigate("/app/tableform/new")}>
-                    Nova tabela
-                  </Button>
-                </ButtonGroup>
-              </InlineGrid>
-
-              <Card roundedAbove="sm" padding={"none"}>
-                <ResourceList
-                  items={tables}
-                  renderItem={renderItem}
-                />
-              </Card>
+          <Card title="Suas tabelas" roundedAbove="sm" padding={"none"}>
+            <BlockStack>
+              <IndexTable
+                resourceName={resourceName}
+                itemCount={tables.length}
+                headings={[
+                  { title: "ID" },
+                  { title: "" },
+                  { title: "Nome" },
+                  { title: "Tipo" },
+                  { title: "Status", alignment: "end" },
+                ]}
+              >
+                {rowsTables}
+              </IndexTable>
             </BlockStack>
           </Card>
         </Layout.Section>
