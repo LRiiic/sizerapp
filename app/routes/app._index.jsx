6fa577cc73718bcea57c09af528396dad8e44d69
Luiz Ricardo <ricardo_santos-2010@hotmail.com>
2024-03-13 19:33:03 -0300

    fix layout list tables

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -6,7 +6,24 @@ import {
   useActionData,
 } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
-import { BlockStack, Box, Card, Image, Layout, Page, Text } from "@shopify/polaris";
+import { 
+  BlockStack,
+  Box,
+  Card,
+  Button,
+  Layout,
+  Page,
+  Text,
+  ResourceList,
+  ResourceItem,
+  Badge,
+  Thumbnail,
+  SkeletonThumbnail,
+  InlineStack,
+  InlineGrid,
+  ButtonGroup,
+} from "@shopify/polaris";
+import {PlusIcon} from '@shopify/polaris-icons';
 import db from "../db.server";
 
 export async function loader({ request }) {
@@ -25,30 +42,70 @@ export const action = async ({ request }) => {
   return null;
 };
 
+
 export default function Index() {
   const navigate = useNavigate();
   const { tables } = useLoaderData();
 
+  function renderItem(item) {
+    const { id, title, content, status, type } = item;
+
+    const url = `/app/tableform/${id}`;
+    let media = (<SkeletonThumbnail size="small" />);
+    if (type === "image") {
+      media = (<Thumbnail product size="small" name={title} source={content} />);
+    }
+
+
+    return (
+      <ResourceItem
+        id={id}
+        onClick={() => navigate(url)}
+        accessibilityLabel={`Ver detalhes para ${title}`}
+        verticalAlignment="center"
+        horizontal
+        media={media}
+      >
+        <InlineStack direction="row" align="space-between">
+          <Text variant="bodyMd" fontWeight="bold" as="h3">
+            {title}
+          </Text>
+          <Badge 
+            tone={status ? "success" : "critical" }
+            progress={status ? "complete" : "incomplete"}
+            size="small"
+            align="right"
+          >
+            {status ? "Ativo" : "Inativo"}
+          </Badge>
+        </InlineStack>
+      </ResourceItem>
+    );
+  }
+
   return (
     <Page>
-      <ui-title-bar title="Shop-Sizer">
-        <button variant="primary" onClick={() => navigate("/app/tableform/new")}>
-          Create table
-        </button>
-      </ui-title-bar>
       <Layout>
         <Layout.Section>
-          <Card title="Suas tabelas" padding="1000">
-            <Text variant="heading2xl" as="h3">
-              Suas tabelas
-            </Text>
-            <BlockStack gap="300">
-              {tables.map((table) => (
-                <Box key={table.id} onClick={() => navigate(`/app/tableform/${table.id}`)}>
-                  <Text variant="bodyMd">{table.title}</Text>
-                  <Image source={table.content} />
-                </Box>
-              ))}
+          <Card title="Suas tabelas" roundedAbove="sm">
+            <BlockStack gap="200">
+              <InlineGrid columns="1fr auto">
+                <Text as="h2" variant="headingSm">
+                  Suas tabelas
+                </Text>
+                <ButtonGroup>
+                  <Button icon={PlusIcon} variant="primary" onClick={() => navigate("/app/tableform/new")}>
+                    Nova tabela
+                  </Button>
+                </ButtonGroup>
+              </InlineGrid>
+
+              <Card roundedAbove="sm" padding={"none"}>
+                <ResourceList
+                  items={tables}
+                  renderItem={renderItem}
+                />
+              </Card>
             </BlockStack>
           </Card>
         </Layout.Section>
